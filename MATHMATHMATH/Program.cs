6863b614468cc9ef4900e7abb1b0eaaca2739fdf
SilverDragon334 <sbaca396@gmail.com>
2022-01-29 17:22:45 -0600

    Add project files.

---
+++ MATHMATHMATH/Program.cs
@@ -0,0 +1,16 @@
+﻿//LETS DO SOME MATHS!!!!
+int MyNum = 67;
+Console.WriteLine(-1 + 4 * MyNum);
+Console.WriteLine((35 + 5) % MyNum);
+Console.WriteLine(14 + -4 * 6 / 12);
+Console.WriteLine(2 + 12 / 6 * 1 - MyNum % 2);
+
+if (MyNum > 4)
+{
+    Console.WriteLine("MyNum is greater than 4");
+}
+
+
+
+
+
